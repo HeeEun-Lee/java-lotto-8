65404816168138e9e72905d2358c5c5520e92e85
HeeEun-Lee <he0960@naver.com>
2025-11-02 21:52:31 +0900

    feat(dto): LottoResponseDto 추가

--- src/main/java/lotto/dto/LottoResponseDto.java
+++ src/main/java/lotto/dto/LottoResponseDto.java
@@ -1,2 +1,21 @@
-package lotto.dto;public class LottoResponseDto {
+package lotto.dto;
+
+import java.util.List;
+
+public class LottoResponseDto {
+
+    private final List<Integer> numbers;
+
+    public LottoResponseDto(List<Integer> numbers) {
+        this.numbers = List.copyOf(numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
