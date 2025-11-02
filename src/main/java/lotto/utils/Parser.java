055ebd77bc2ccbf00d7c5247300cf0d026e1e2ff
HeeEun-Lee <he0960@naver.com>
2025-11-02 20:54:20 +0900

    feat(utils): 쉼표 기준으로 구분하는 Parser 구현

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,2 +1,25 @@
-package lotto.utils;public class Parser {
+package lotto.utils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import lotto.exception.ErrorMessage;
+
+public class Parser {
+    private static final Pattern VALID_PATTERN =
+            Pattern.compile("^\\s*\\d{1,2}(\\s*,\\s*\\d{1,2}){5}\\s*$");
+
+    public static List<Integer> parse(String input) {
+        if (!VALID_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_FORMAT.getMessage());
+        }
+
+        List<Integer> numbers = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
+
+        return numbers;
+    }
+
 }
