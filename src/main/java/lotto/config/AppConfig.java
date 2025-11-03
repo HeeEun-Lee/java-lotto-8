b445fbe06fabff57d14eef41cd4b798f1d86048c
HeeEun-Lee <he0960@naver.com>
2025-11-03 16:49:40 +0900

    chore: config 패키지 및 설정 클래스 추가

---
+++ src/main/java/lotto/config/AppConfig.java
@@ -0,0 +1,25 @@
+package lotto.config;
+
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class AppConfig {
+
+    public LottoController lottoController() {
+        return new LottoController(lottoService(), inputView(), outputView());
+    }
+
+    public LottoService lottoService() {
+        return new LottoService();
+    }
+
+    public InputView inputView() {
+        return new InputView();
+    }
+
+    public OutputView outputView() {
+        return new OutputView();
+    }
+}
