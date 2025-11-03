3c4a9854076c47e1a838ee2f34db0cd28b8fa623
HeeEun-Lee <he0960@naver.com>
2025-11-03 16:44:45 +0900

    feat(utils): 잘못된 입력 시 재시도할 수 있는 InputRetryHandler 추가

---
+++ src/main/java/lotto/utils/InputRetryHandler.java
@@ -0,0 +1,16 @@
+package lotto.utils;
+
+import java.util.function.Supplier;
+
+public class InputRetryHandler {
+
+    public static <T> T get(Supplier<T> inputFunction) {
+        while (true) {
+            try {
+                return inputFunction.get();  // 입력 시도
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            }
+        }
+    }
+}
